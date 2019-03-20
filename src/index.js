d9305e2733f03cb3237cc9ca75652b05eb2e9094
Nastya Ivanova <shalygai.a@gmail.com>
2019-03-20 20:49:06 +0300

    feat(advanced-zeros): get trailing number of zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let numberOfZeros = number;
+  let primeNumber = base;
+
+  for (let i = 2; i <= base; i++) {
+    if ( primeNumber % i === 0) {
+      let count = 0;
+      while ( primeNumber % i === 0) {
+        primeNumber /= i;     
+        count++;
+      }
+
+      let power = 0;
+      let remainder  = Math.floor(number / i);
+      while (remainder  > 0) {
+        power += remainder ;
+        remainder  = Math.floor(remainder / i);
+      }
+      numberOfZeros = Math.min(numberOfZeros, Math.floor(power / count))
+    }
+  }
+
+  return numberOfZeros;
 }
